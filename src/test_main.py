eefcca9a77a59a3e1236e64a5ed1ab4e8435fb94
NguyenN95 <nguyennguyen7595@gmail.com>
2023-02-22 18:55:11 -0500

    add more test, restrict to only  ver 3.10

--- src/test_main.py
+++ src/test_main.py
@@ -9,3 +9,45 @@ def test_root():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"Hello": "World"}
+
+
+def test_detect_chair_1():
+    files = {"file": open("./test_images/Chair/chair 1.png", "rb")}
+    response = client.post("/predicts-image", files=files)
+    assert response.status_code == 200
+    assert response.json() == {"label": "Chair"}
+
+
+def test_detect_chair_2():
+    files = {"file": open("./test_images/Chair/chair 2.png", "rb")}
+    response = client.post("/predicts-image", files=files)
+    assert response.status_code == 200
+    assert response.json() == {"label": "Chair"}
+
+
+def test_detect_bed_1():
+    files = {"file": open("./test_images/Bed/bed 1.jpg", "rb")}
+    response = client.post("/predicts-image", files=files)
+    assert response.status_code == 200
+    assert response.json() == {"label": "Bed"}
+
+
+def test_detect_bed_2():
+    files = {"file": open("./test_images/Bed/bed 2.jpg", "rb")}
+    response = client.post("/predicts-image", files=files)
+    assert response.status_code == 200
+    assert response.json() == {"label": "Bed"}
+
+
+def test_detect_sofa_1():
+    files = {"file": open("./test_images/Sofa/sofa 1.jpg", "rb")}
+    response = client.post("/predicts-image", files=files)
+    assert response.status_code == 200
+    assert response.json() == {"label": "Sofa"}
+
+
+def test_detect_sofa_2():
+    files = {"file": open("./test_images/Sofa/sofa 2.jpg", "rb")}
+    response = client.post("/predicts-image", files=files)
+    assert response.status_code == 200
+    assert response.json() == {"label": "Sofa"}
